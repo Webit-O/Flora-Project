04023d59a680f4d8197638543cfc325d51c9187e
Samuel <samusr@gmail.com>
2024-12-28 16:33:37 +0000

    Configured prettier and favicon

--- front-florasanchez/app/routes/home.tsx
+++ front-florasanchez/app/routes/home.tsx
@@ -1,13 +1,21 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'Flora Sanchez | Inicio | Entrenamientos y Nutirición' },
+    {
+      name: 'description',
+      content:
+        'Flora Sánchez, entrenadora y nutricionista en Las Palmas de Gran Canaria, te ayuda a mejorar tu rendimiento deportivo y salud. Especialista en nutrición deportiva, dietética y entrenamientos personalizados.',
+    },
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <h1>Hola</h1>
+      <p>Esto es una prueba</p>
+    </>
+  )
 }
