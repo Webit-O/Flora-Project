99fc6f09bf1da85c7e2d87ceb8dc6ac8d9ad46cf
Samuel <samusr@gmail.com>
2025-01-15 19:35:34 +0000

    Finished navbar component

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -1,15 +1,40 @@
-import { NavLink } from 'react-router'
+import { useState } from 'react'
+import { NavLink, Link } from 'react-router'
 import { navbarLinks } from '../../constants/navbarLinks'
 import './Navbar.css'
 
 export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <header className='navbar'>
       <div className='navbar__logo'>
         <img src='./images/logo.webp' alt='Logo' />
       </div>
 
-      <nav>
+      <div
+        className={`navbar__toggle ${isMenuOpen ? 'navbar__toggle--active' : ''}`}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <span className='line'></span>
+        <span className='line'></span>
+        <span className='line'></span>
+      </div>
+
+      {/* NAV MOBILE */}
+
+      <nav className={`navbar__mobile ${isMenuOpen ? 'open' : ''}`}>
+        <ul>
+          {navbarLinks.map(({ id, title, path }) => (
+            <li key={id}>
+              <Link to={path}>{title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* NAV DESKTOP */}
+      <nav className='navbar__desktop'>
         <ul className='navbar__list'>
           {navbarLinks.map(({ id, title, path }) => (
             <li key={id}>
